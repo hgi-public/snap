86be42348a47244ae4bd940a2815c4353c364b88
acastellane <alexandre.castellane@fr.ibm.com>
2017-09-14 12:56:39 +0000

    tmp file with '#' will be ignored, and an empty message aborts the commit.

--- edu_set_init_variables.sh
+++ edu_set_init_variables.sh
@@ -3,7 +3,7 @@ echo please run from home git clone https://github.com/ibm-capi/pslse
 echo please run from home git clone https://github.com/open-power/snap
 echo "Setting Xilinx path and sourcing init script"
 export xilinx_root=/opt/Xilinx
-source /opt/Xilinx/Vivado/2016.4/settings64.sh
+. /opt/Xilinx/Vivado/2016.4/settings64.sh
 export XILINXD_LICENSE_FILE=2100@cloudsynthesismaster
 echo export PSLSE_ROOT=/home/opuser/pslse
 export PSLSE_ROOT=/home/opuser/pslse
